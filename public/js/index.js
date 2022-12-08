dd083697fe423a578ff1b8d2d68bb78ad0bfe8e3
LeeMyeongjae <audwo8140@gmail.com>
2022-12-08 11:56:17 +0900

    js적용 및 이름 커서변경

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,20 @@
-const test = 10;
+const headuserNameElement = document.querySelector('.user');
+const userNameElement = document.querySelector('#name');
 
-console.log(test);
+const localUserName = localStorage.getItem('userName');
+
+const setUserNameInnerHtml = (name) => {
+  headuserNameElement.innerHTML = '${name} <span>님</span>';
+  userNameElement.textContent = name;
+};
+
+if (localUserName) {
+  setUserNameInnerHtml(localUserName);
+}
+
+userNameElement.onclick = () => {
+  const userName = prompt('이름을 입력해 주세요.');
+  localStorage.setItem('userName', userName);
+
+  setUserNameInnerHtml(userName);
+};
