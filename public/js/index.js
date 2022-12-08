29f9e64412d5c767c8b977805641bbaaa221474c
LeeMyeongjae <audwo8140@gmail.com>
2022-12-08 12:48:23 +0900

    유효성 검사 전

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,36 @@
 const headuserNameElement = document.querySelector('.user');
 const userNameElement = document.querySelector('#name');
+const userUVidElement = document.querySelector('#UVid');
+const userEmailElement = document.querySelector('#Email');
 
 const localUserName = localStorage.getItem('userName');
+const localUserEmail = localStorage.getItem('userEmail');
+const localUserUVid = localStorage.getItem('userUVid');
+
 
 const setUserNameInnerHtml = (name) => {
   headuserNameElement.innerHTML = `${name} <span>님</span>`;
   userNameElement.textContent = name;
 };
 
+const setUserUVidInnerHtml = (UVid) => {
+  userUVidElement.textContent = UVid;
+};
+
+const setUserEmailInnerHtml = (Email) => {
+  userEmailElement.textContent = Email;
+};
+
 if (localUserName) {
   setUserNameInnerHtml(localUserName);
+}
+
+if (localUserEmail) {
+  setUserUVidInnerHtml(localUserEmail);
+}
+
+if (localUserUVid) {
+  setUserEmailInnerHtml(localUserUVid);
 }
 
 userNameElement.onclick = () => {
@@ -17,4 +38,18 @@ userNameElement.onclick = () => {
   localStorage.setItem('userName', userName);
 
   setUserNameInnerHtml(userName);
+};
+
+userUVidElement.onclick = () => {
+  const userUVid = prompt('학번을 입력해 주세요.');
+  localStorage.setItem('userUVid', userUVid);
+
+  setUserUVidInnerHtml(userUVid);
+};
+
+userEmailElement.onclick = () => {
+  const userEmail = prompt('이메일을 입력해 주세요.');
+  localStorage.setItem('userEmail', userEmail);
+
+  setUserEmailInnerHtml(userEmail);
 };
